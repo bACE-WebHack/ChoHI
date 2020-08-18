a13715ee243dd1c9ca31d32c9a045252d3b07e5f
root <gksdlrdl36@naver.com>
2020-08-18 12:26:24 +0000

    reg

--- register.php
+++ register.php
@@ -1,5 +1,71 @@
 <?php
 
+require_once "config.php";
+
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+	if(empty(trim($_POST['id']))){
+		$id_err = "아이디를 입력해 주세요.";
+	}
+	else{
+		$id = $_POST['id'];
+	}
+
+	if(empty(trim($_POST['name']))){
+		$name_err = "이름을 입력해 주세요.";
+	}
+	else{
+		$name = $_POST['name'];
+	}
+
+	if(empty(trim($_POST['pw']))){
+		$pw_err = "패스워드를 입력해 주세요.";
+	}
+	else{
+		$pw = $_POST['pw'];
+	}
+
+	if($_POST['pw'] != $_POST['pwck']){
+		$pwck_err = "패스워드 값이 다릅니다.";
+	}
+
+	if(empty(trim($_POST['pn']))){
+		$pn_err = "휴대폰 번호를 입력해주세요.";
+	}
+	else{
+		$pn = $_POST['pn'];
+	}
+
+	if(empty(trim($_POST['sex']))){
+		$sex_err = "성별을 체크해주세요.";
+	}
+	else{
+		if($_POST['sex']=="남자"){
+			$sex=1;
+		}
+		else{
+			$sex=2;
+		}
+	}
+
+
+	if(empty($id_err)&&empty($pw_err)&&empty($pwck_err)&&empty($pn_err)&&empty($sex_err)){
+
+		$sql = "insert into userinfo value('".$id."','".$name."','".$pn."','".$pw."','".$sex."');";
+		echo $sql;
+		$res = mysqli_query($link,$sql);
+		
+
+		echo "<script>alert('회원가입이 완료되었습니다.');document.location.href='login.php';</script>";
+	
+	}	
+
+
+}
+
+
+
 
 ?>
 <!DOCTYPE html>
@@ -17,6 +83,11 @@
 	<label>아이디</label>
 	</td><td>
 	<input type="text" name="id" placeholder="ID"><br>
+	</td></tr>
+	<tr><td>
+	<label>이름</label>
+	</td><td>
+	<input type="name" name="name" placeholder="NAME"><br>
 	</td></tr>
 	<tr><td>
 	<label>비밀번호</label>
