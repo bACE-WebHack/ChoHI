675a7e52f316bcd7328e55eb9003e32bc8dd724a
root <gksdlrdl36@naver.com>
2020-08-18 09:59:56 +0000

    1

--- login.php
+++ login.php
@@ -1,6 +1,26 @@
 <?php
 
 
+	require_once "config.php";
+
+	if(isset($_POST['id']) && isset($_POST['password'])){
+
+		$id = $_POST['id'];
+		$pw = $_POST['password'];
+	}
+
+	$sql="select * from userinfo where id='$id'&&PW='$pw'";
+	
+	if($res=mysqli_fetch_array(mysqli_query($link,$sql))){
+		
+		$_SESSION['userid']=$id;
+		
+		echo "<script>alert('로그인 성공!');document.location('welcom.php');<script>";
+	}
+	else{
+		echo "<script>alert('아이디나 패스워드를 확인해주세요.');</script>";
+	}
+
 
 
 
@@ -15,11 +35,11 @@
 <body>
 <h2>Login Page!</h2>
 <form method="POST">
-<input type="text" name="id" placeholder="ID">
-<input type="password" name="password" placeholder="password">
+<label>ID</label>
+<input type="text" name="id" placeholder="ID"><br>
+<label>PW</label>
+<input type="password" name="password" placeholder="password"><br><br>
 <button type="submit">Login</button>
-<br>
-<br>
 <button type="button" onclick="location.href='register.php'">register</button>
 
 </body>
--- register.php
+++ register.php
@@ -9,6 +9,41 @@
 <title>Register!</title>
 </head>
 <body>
+<h1 align="center">회원가입</h1>
 
+	<form method="post">
+	<table border="1" width="500" height="300" align="center">
+	<tr><td>
+	<label>아이디</label>
+	</td><td>
+	<input type="text" name="id" placeholder="ID"><br>
+	</td></tr>
+	<tr><td>
+	<label>비밀번호</label>
+	</td><td>	
+	<input type="password" name="pw" placeholder="PW"><br>
+	</td></tr>
+	<tr><td>
+	<label>비밀번호 확인</label>
+	</td><td>	
+	<input type="password" name="pwck" placeholder="PW Confirm"><br>
+	</td></tr>
+	<tr><td>
+	<label>휴대폰 번호</label>
+	</td><td>
+	<input type="text" name="pn" placeholder="Phone number"><br>
+	</td></tr>
+	<tr><td>
+	<label>성별</label>
+	</td><td>
+	남자<input type="radio" id="sex" name="sex" value="남자">
+	여자<input type="radio" id="sex" name="sex" value="여자">
+	</td></tr>
+	</table>
+	<div align="center">
+	<button type="submit" name="register">Register</button>
+	<button type="button" name="cancle" onclick="location.href='login.php'">취소</button>
+	</div>
+	</form>
 </body>
 </html>
